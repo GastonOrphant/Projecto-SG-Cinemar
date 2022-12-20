480db9af7efe5a7492c7da792973051513a23a20
GastonOrphant <gaston.orphant@hotmail.com>
2022-12-19 21:15:06 -0300

    agregada tarjeta a la clase usuario

--- app.py
+++ app.py
@@ -2,58 +2,104 @@ import tkinter as tk
 from clases.usuario import Usuario 
 
 def login():
-    # create the main window
+    # crear la ventana principal para el login
     root = tk.Tk()
     root.wm_iconbitmap('assets/favicon.png')
     root.title("Login")
-
     # labels
     mail_label = tk.Label(root, text="Mail:")
     password_label = tk.Label(root, text="Password:")
-
     # forms
     mail_field = tk.Entry(root)
     password_field = tk.Entry(root, show="*")
     # boton de ingresar
     submit_button = tk.Button(root, text="Ingresar")
-
-    # create a function to handle the form submission
     def handle_submit():
-        # get the values entered in the form fields
+        # toma los valores ingresados en las fields
         mail = mail_field.get()
         password = password_field.get()
-        # print the values to the console
-        print("Mail:", mail)
-        print("Password:", password)
-
-    # bind the submit button to the submit function
+        # verifica que el mail ingresado este en la base de datos
+        if Usuario.usuarioEstaEnBD(mail):
+            if Usuario.validarUsuario(mail, password):
+                # realiza el login y pasa a ventana de reservas
+                pass
+        else:
+            pass
+            # CARTEL USUARIO INCORRECTO
+    # Bindea el boton submit a la funcion handle_submit
     submit_button.bind("<Button-1>", handle_submit)
-
+    # Boton de Registro
+    register_button = tk.Button(root, text="Registrarse!")
+    def handle_register():
+        root.quit()
+        registro()
+    register_button.bind("<Button-1>", handle_register)   
     # posicionar los widgets
     mail_label.grid(row=0, column=0)
     mail_field.grid(row=0, column=1)
     password_label.grid(row=1, column=0)
     password_field.grid(row=1, column=1)
     submit_button.grid(row=2, column=1)
-
+    register_button.grid(row=2, column=0)
     # start the main loop
     root.mainloop()
 
 
-#EJEMPLO PARA REALIZAR EL REGISTRO DE UN USUARIO
+# Realiza el registro de un usuario
 def registro():
-    nombre = input("Ingrese nombre de usuario: ")
-    mail = input("Ingrese su mail: ")
-    contrasenia = input("Ingrese una contraseña: ")
-    tipo = input("Ingrese el tipo de usuario que es (0 = Cliente, 1 = Admin): ")
-    usuario = Usuario(nombre, mail, contrasenia, tipo)
-    if usuario.usuarioEstaEnBD(mail):
-        print("Usuario ya registrado.")
-    else:    
-        usuario.insertarEnBD()
+    root = tk.Tk()
+    root.wm_iconbitmap('assets/favicon.png')
+    root.title("Registro de usuario")
+    nombre_label = tk.Label(root, text="Ingrese un nombre de usuario:")
+    mail_label = tk.Label(root, text="Ingrese un Mail para registrarse:")
+    password_label = tk.Label(root, text="Ingrese una contraseña: ")
+    tipo_usuario_label = tk.Label(root, text="Ingrese su tipo de usuario (0 = Cliente, 1 = Admin):")  #Cambiar a botón mas adelante
+    tarjeta_label = tk.Label(root, text="Cuenta con tarjeta de descuentos(0: No, 1: Si): ")
+    nombre_field = tk.Entry(root)
+    mail_field = tk.Entry(root)
+    password_field = tk.Entry(root, show="*")
+    tipo_usuario_field = tk.Entry(root)
+    tarjeta_field = tk.Entry(root)
+    submit_button = tk.Button(root, text="Registrarse")
+    def handle_submit():
+        # toma los valores ingresados en las fields
+        nombre = nombre_field.get()
+        mail = mail_field.get()
+        contrasenia = password_field.get()
+        tipo = tarjeta_field.get()
+        usuario = Usuario(nombre, mail, contrasenia, tipo)
+        if usuario.usuarioEstaEnBD(mail):
+            print("Usuario ya registrado.")
+        else:    
+            usuario.insertarEnBD()
+    # bind the submit button to the submit function
+    submit_button.bind("<Button-1>", handle_submit)
+    # posicionar los widgets
+    nombre_label.grid(row=0, column=0)
+    nombre_field.grid(row=0, column=1)
+    mail_label.grid(row=1, column=0)
+    mail_field.grid(row=1, column=1)
+    password_label.grid(row=2, column=0)
+    password_field.grid(row=2, column=1)
+    tipo_usuario_label.grid(row=3, column=0)
+    tipo_usuario_field.grid(row=3, column=1)
+    tarjeta_label.grid(row=4, column=0)
+    tarjeta_field.grid(row=4, column=1)
+    submit_button.grid(row=5, column=1)
+    # start the main loop
+    root.mainloop()        
 
+def descuentos(precioEntrada, fecha):
+    '''Lunes y Miércoles: 20% | Martes y Jueves: 15% | Viernes, Sábados y Domingos: 10% 
+                    Siendo modificable según los directivos.'''
+    if (fecha == 'Lunes') or (fecha == 'Miercoles'):
+        descuento = 0.2
+    elif (fecha == 'Martes') or (fecha == 'Jueves'):
+        descuento = 0.15
+    else:
+        descuento = 0.1
+    return(precioEntrada - precioEntrada*descuento)             
 
 
 if __name__ == "__main__":
-    #login()
-    registro()    
+    login()
--- clases/usuario.py
+++ clases/usuario.py
@@ -1,10 +1,11 @@
 from database.conexion import Conexion_BD
 class Usuario:
-    def __init__(self, nombreUsuario, mail, contrasenia, tipo):
+    def __init__(self, nombreUsuario, mail, contrasenia, tipo, tarjeta):
         self.nombreUsuario = nombreUsuario
         self.mail = mail
         self.contrasenia = contrasenia
         self.tipo = tipo # 0 si es cliente | 1 si es admin
+        self.tarjeta = tarjeta # 0 si no tiene | 1 si tiene
 
     def __str__(self):
         if self.tipo == 1:
@@ -14,9 +15,9 @@ class Usuario:
         return(f"Nombre: {self.nombreUsuario}, mail: {self.mail}, tipo de usuario: {tipoUsuario}")
 
     def insertarEnBD(self, conn):
-        '''Inserta un  usuario en la base de datos'''
+        '''Inserta un usuario en la base de datos'''
         conn = Conexion_BD()
-        conn.consult(f'INSERT INTO usuario VALUES ({self.nombreUsuario} ,{self.mail}, {self.contrasenia}, {self.tipo})')
+        conn.consult(f'INSERT INTO usuario VALUES ({self.nombreUsuario} ,{self.mail}, {self.contrasenia}, {self.tipo}, {self.tarjeta})')
         conn.commit()
 
     def usuarioEstaEnBD(self, mail):
