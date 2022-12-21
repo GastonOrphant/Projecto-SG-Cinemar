79f0ad19a9d1232db8830aa1e9ea8c07f898f590
NicolasBarrientos <nbarrientos.nubicom@gmail.com>
2022-12-21 16:54:54 -0300

    clases nicolas

---
+++ clases/funcion.py
@@ -0,0 +1,15 @@
+class funcion ():
+    def __init__ (self, idSala, fecha, horario, estado):
+        self.idSala=idSala
+        self.fecha= fecha
+        self.horario= horario
+        self.estado=estado
+
+    def insertarFuncion (self, idSala, fecha, horario, estado):
+        pass
+    def mostrarFunciones (self):
+        pass
+    def mostrarUnaFuncion(self, idFUncion):
+        pass
+    def eliminarUnaFuncion (self, idFuncion):
+        pass
---
+++ database/conexion.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+class Conexion_BD():
+    def __init__(self):
+        self.conexion = sqlite3.connect('database/Cinemar.db')
+        self.cursor = self.conexion.cursor()
+
+    def consult(self, consulta):
+      if consulta.upper().startswith("SELECT"):
+        data = self.cursor.execute(consulta)
+        return data
+      else:
+        self.cursor.execute(consulta)
+
+    def commit(self):
+        self.conexion.commit()
+
+    def close(self):
+        self.conexion.close()
+
+        
